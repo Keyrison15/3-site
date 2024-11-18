fd3bc03b3553d27343b4223e016c7dea51dd34a9
Keyrison15 <160617717+Keyrison15@users.noreply.github.com>
2024-11-18 11:52:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Nascimento',
+    'Em que ano Lionel Messi nasceu?',
+    '24 de junho de 1987'
+)
+
+criaCartao(
+    'Titulos',
+    'Quantos titulos Messi tem?',
+    '44 títulos oficiais como profissional'
+)
+
+criaCartao(
+    'Gols',
+    'Quantos gols Messi tem na carreira?',
+    '849 gols até o presente momento'
+)
+
+criaCartao(
+    'Carreira',
+    'Quais são os times que o Messi já jogou?',
+    'Barcelona, PSG, Argentina e agora Inter Miami'
+)
+criaCartao(
+    'Carreira',
+    ' Qual o nome completo do Messi? ',
+    'Lionel Andrés Messi Cuccitini'
+)
+criaCartao(
+    'Carreira',
+    'Qual a altura Messi tem? ',
+    '1,69'
+)
